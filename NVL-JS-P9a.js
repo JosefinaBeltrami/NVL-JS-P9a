7a469775f044746f1e99769cd120e1a30f8a2f92
Maria Josefina Beltrami <31435391+JosefinaBeltrami@users.noreply.github.com>
2019-05-17 15:05:22 +0200

    Add files via upload

---
+++ NVL-JS-P9a.js
@@ -0,0 +1,6 @@
+var b = ["elemento 1 de la matrix", "segundo_elemento", [3,"elemento 4 de la matrix", "elemento 5 de la matrix general, elemento 3 de la 2 matrix"]];
+
+alert(b[1]);
+alert(b.length);
+alert(b[2].length);
+alert(b[2][2]);
